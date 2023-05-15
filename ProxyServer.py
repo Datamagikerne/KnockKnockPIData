fea15921337a1a67a769bf7f73b8d61f5629a7c8
olliwh <98967791+olliwh@users.noreply.github.com>
2023-05-15 18:49:16 +0200

    Refactor and DRY

--- ProxyServer.py
+++ ProxyServer.py
@@ -25,44 +25,52 @@ testServername = '127.0.0.1'
 print("The server is ready")
 count = 1
 cet = pytz.timezone('CET')
-while True:
+
+
+def recieve_img():
     file = open(f'c:/temp/qr{count}.jpg', 'wb')
     filedata, clientAddress = serverSocket.recvfrom(65507)
     print(clientAddress)
     print("Receiving...")
     file.write(filedata)
     file.close()
-    try:
-        img = cv2.imread(f'c:/temp/qr{count}.jpg')
-        id = decode(img)[0].data.decode('utf-8')
-        ArrivalTime = datetime.datetime.now(cet).strftime("%Y-%m-%dT%H:%M:%SZ")
-        data = {
-            "id": 0,
-            "arrivalTime": ArrivalTime,
-            "qrCode": id,
-            "name": "string"
-        }
-        print(id)
-        print(ArrivalTime)
-        response = requests.post(REST_URL, json=data)
-        # print(response.text)
-        if response.ok:
-            responseOK = 'ok'
-            print((responseOK))
-            respSocket.sendto(responseOK.encode(), (Piservername, resport))
-            respSocket.sendto(responseOK.encode(), (testServername, resport))
-        if response.status_code == 400:
-            responseBadR = '400 Bad request'
-            print(responseBadR)
-            respSocket.sendto(responseBadR.encode(), (Piservername, resport))
-            respSocket.sendto(responseBadR.encode(), (testServername, resport))
 
+def get_data():
+    img = cv2.imread(f'c:/temp/qr{count}.jpg')
+    id = decode(img)[0].data.decode('utf-8')
+    ArrivalTime = datetime.datetime.now(cet).strftime("%Y-%m-%dT%H:%M:%SZ")
+    data = {
+        "id": 0,
+        "arrivalTime": ArrivalTime,
+        "qrCode": id,
+        "name": "string"
+    }
+    print(id)
+    print(ArrivalTime)
+    return data
+
+def get_response(rest_response):
+    if rest_response.ok:
+        responseOK = 'ok'
+        print((responseOK))
+        respSocket.sendto(responseOK.encode(), (Piservername, resport))
+        respSocket.sendto(responseOK.encode(), (testServername, resport))
+    if rest_response.status_code == 400:
+        responseBadR = '400 Bad request'
+        print(responseBadR)
+        respSocket.sendto(responseBadR.encode(), (Piservername, resport))
+        respSocket.sendto(responseBadR.encode(), (testServername, resport))
+
+while True:
+    recieve_img()
+    try:
+        response = requests.post(REST_URL, json=get_data())
+        # print(response.text)
+        get_response(response)
         count += 1
     except:
         print('Not a qr code')
         response = 'not a QR code'
         respSocket.sendto(response.encode(), (Piservername, resport))
         respSocket.sendto(response.encode(), (testServername, resport))
-
-
 
